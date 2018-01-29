91586dff10012080011d527914905489dd73688d
Chiefautoparts <chiefautoparts@outlook.com>
2018-01-29 04:34:23 -0800

    updates to v4.2

--- canvas2d.js
+++ canvas2d.js
@@ -5,7 +5,16 @@ function Canvas2D($canvas)
     width = $canvas[0].width,
     height = $canvas[0].height,
     pageOffset = $canvas.offset();
+
   $(window).resize(function() { pageOffset = $canvas.offset() });
+
+  // Set defaults
+  context.lineWidth = 4;
+  context.strokeStyle = "black";
+  context.fillStyle = "black";
+  context.globalAlpha = 1.0;
+  context.lineJoin = "round";
+  context.lineCap = "round";
 
   this.getCanvasPoint = function(pageX, pageY)
   {
@@ -14,6 +23,49 @@ function Canvas2D($canvas)
       x: pageX - pageOffset.left,
       y: pageY - pageOffset.top
     }
+  };
+
+  this.savePen = function()
+  {
+    context.save();
+    return this;
+  };
+
+  this.restorePen = function()
+  {
+    context.restore();
+    return this;
+  };
+
+  this.penWidth = function(newWidth)
+  {
+    if (arguments.length)
+    {
+      context.lineWidth = newWidth;
+      return this;
+    }
+    return context.lineWidth;
+  };
+
+  this.penColor = function(newColor)
+  {
+    if (arguments.length)
+    {
+      context.strokeStyle = newColor;
+      context.fillStyle = newColor;
+      return this;
+    }
+    return context.strokeStyle;
+  };
+
+  this.penOpacity = function(newOpacity)
+  {
+    if (arguments.length)
+    {
+      context.globalAlpha = newOpacity;
+      return this;
+    }
+    return context.globalAlpha;
   };
 
   this.clear = function()
